9f0d0c9cd3208ead0a8316bc8e5685f59e13d32d
JohnnyJ03 <Healthbyjjohnson@gmail.com>
2023-08-14 12:44:02 -0400

    initla commit

--- script.js
+++ script.js
@@ -1,202 +1,103 @@
-// GLOBAL VARIABLES
-var searchButton = document.getElementById("searchButton");
-var searchTextEl = document.getElementById("cityName");
-var fiveDayBoxes = document.getElementsByClassName("rounded");
-var clearBoxes = document.getElementById("clearText")
-var clearBorder = document.getElementById("clearBorder")
-// Selects City Name Box Header
-var cityNameInfo = document.getElementById("cityNameInfo").firstChild;
-// Selects list of city name elements
-var cityList = document.getElementById("cityList");
+var APIkey = "f54fd17086de3432994e4ebbcd1d1639"
+var searchBtn = $('#search-btn')
+var city = $("#searchbox")
+var currentDate = dayjs().format('M/D/YY')
+var currentCity = $('#today')
+var currentTemp = $('#todayTemp')
+var currentWind = $('#todayWind')
+var currentHumid = $('#todayHumid')
+var savedSearches = $('#saved-search')
 
-// // Clears out elements... how do I make it come back?
-// cityNameInfo.textContent = `Type the name of a city in the search bar!`;
-// cityList.textContent = ``;
-// clearBoxes.textContent = ``;
+//API call to fetch and display current weather in searched city
+function weatherToday() {
+    var requestURL = `https://api.openweathermap.org/data/2.5/weather?q=${city.val()}&appid=${APIkey}&units=imperial`;
 
-function showElements() {
-  var x = document.getElementById("hiddenEl");
-  if (x.style.display === "block") {
-    x.style.display = "none";
-  } else {
-    x.style.display = "block";
-  }
-}
-
-// Click Event for Search Button 
-searchButton.addEventListener(`click`, inputValidate)
-// Validates input
-function inputValidate(event) {
-  if (!searchTextEl.value) {
-    return;
-  }
-  event.preventDefault();
-
-  var search = searchTextEl.value.trim();
-  getWeather(search);
-  searchTextEl.value = " ";
-}
-
-// Gets ALL weather data from API
-function getWeather(city) {
-
-  // Get city coordinates via API
-  getCoordinates(city);
-
-  function getCoordinates(search) {
-    var apiCityURL = `http://api.openweathermap.org/geo/1.0/direct?q=${search}&limit=5&appid=b98ec477e026dbcba46222f669c18788`
-    fetch(apiCityURL)
-      .then(function (response) {
-        // Stores response in JSON object
-        response.json()
-          .then(function (data) {
-            // Retrieves first city result of our data object
-            getFutureWeather(data[0])
-          })
-      })
-  }
-
-  // Uses city coordinates to get 5 day weather forecast
-  function getFutureWeather(pullData) {
-    // Same as (data[0])
-    var { lat, lon } = pullData
-    var cityName = pullData.name
-    var apiCoordinateURL = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=b98ec477e026dbcba46222f669c18788&units=imperial`
-    fetch(apiCoordinateURL)
-      .then(function (coordinateResponse) {
-        coordinateResponse.json()
-          .then(function (data) {
-            // create empty varible to push 5 days of data into
-            var emptyFutureVariable = []
-            var emptyCurrentVariable = []
-            var emptyAllFutureVariable = []
-            var dataList = data.list;
-
-            // For current forecast
-            var pushCurrentData = dataList[0]
-            emptyCurrentVariable.push(pushCurrentData)
-
-            // For future forecast days (5)
-            for (let i = 1; i < dataList.length; i += 8) {
-              var pushFutureData = dataList[i];
-              emptyFutureVariable.push(pushFutureData);
-            }
-
-            // For all Future Forecast Days
-            var pushAllFutureData = dataList[i];
-            emptyAllFutureVariable.push(pushAllFutureData)
-
-            // Create new function for printing the results in HTML
-            printForecastData(emptyFutureVariable)
-
-            // Create new function for printing date in HTML
-            printFutureForecastDate(emptyAllFutureVariable)
-
-            fetch(apiCoordinateURL)
-              .then(function (weatherResponse) {
-                weatherResponse.json()
-                  .then(function (weatherData) {
-
-                    // CURRENT DAY DISPLAY
-
-                    //Gets the current weather icon
-                    let locationIcon = document.querySelector('.weather-icon');
-                    const icon = emptyCurrentVariable[0].weather[0].icon;
-                    locationIcon.innerHTML = `<img src="https://openweathermap.org/img/wn/${icon}.png">`
-
-                    // Display the City Name, Date, and cityCurrentWeatherIcon on cityNameInfo
-                    cityNameInfo.textContent = weatherData.city.name + ` ` + dayjs().format(`MMM D, YYYY`)
-
-                    // Display the Temp, Wind, and Humidity on cityList
-                    cityList.children[0].textContent = `Temp: ` + weatherData.list[0].main.temp + ` F`
-                    cityList.children[1].textContent = `Wind Speed: ` + weatherData.list[0].wind.speed + ` m/s`
-                    cityList.children[2].textContent = `Humidity: ` + weatherData.list[0].main.humidity + `%`
-
-                  })
-
-              })
-
-          })
-      })
-  }
-  
-  function printFutureForecastDate (forecastDateData) {
-    $('#day1').text(dayjs.unix(data.list[7].dt).format('M/D/YY'))
-    $('#day2').text(dayjs.unix(data.list[15].dt).format('M/D/YY'))
-    $('#day3').text(dayjs.unix(data.list[23].dt).format('M/D/YY'))
-    $('#day4').text(dayjs.unix(data.list[31].dt).format('M/D/YY'))
-    $('#day5').text(dayjs.unix(data.list[39].dt).format('M/D/YY'))
-
-  }
-
-  // Pass through emptyVariable data into forecastData params
-  function printForecastData(forecastData) {
-    // console.log(forecastData)
-    // Changes the Date
-    // getFutureDates();
-  
-    for (let i = 0; i < fiveDayBoxes.length; i++) {
-      // Changes Content of Date on 5 Day Forecast Section
-      // Changes the temperature
-      fiveDayBoxes[i].children[1].children[0].textContent = `Temp: ` + forecastData[i].main.temp + ` F`
-      // Changes the wind speed
-      fiveDayBoxes[i].children[1].children[1].textContent = `Wind Speed: ` + forecastData[i].wind.speed + ` m/s`
-      // Changes the humidity
-      fiveDayBoxes[i].children[1].children[2].textContent = `Humidity: ` + forecastData[i].main.humidity + `%`
-    }
-      // HELP- how do I push the date into my h3
-  //   function getFutureDates() {
-  //     fetch()
-  //     // Declare a date variable set to current date/time:
-  //     let dt = new Date();
-  //     // Box 1
-  //     var dt0 = dt.setDate(dt.getDate() + 1)
-  //     dt0 =  dayjs().format('M/D/YYYY')
-  //     console.log(dt0)
-  //     let string1 = dt0.toString();
-  //     fiveDayBoxes[0].children[0].innerHTML = `<h3>${string1}</h3>`
-
-  //     // Box 2
-  //     var dt1 = dt.setDate(dt.getDate() + 2)
-  //     dt1 =  dayjs().format('M/D/YYYY')
-  //     fiveDayBoxes[1].children[0].textContent = dt1
-
-  //     // Box 3
-  //     var dt2 = dt.setDate(dt.getDate() + 3)
-  //     dt2 =  dayjs().format('M/D/YYYY')
-  //     fiveDayBoxes[2].children[0].textContent = dt2
-
-  //     // Box 4
-  //     var dt3 = dt.setDate(dt.getDate() + 4)
-  //     dt3 =  dayjs().format('M/D/YYYY')
-  //     fiveDayBoxes[3].children[0].textContent = dt3
-
-  //     // Box 5
-  //     var dt4 = dt.setDate(dt.getDate() + 5)
-  //     dt4 =  dayjs().format('M/D/YYYY')
-  //     fiveDayBoxes[4].children[0].textContent = dt4
-  //   }
-  // }
-  // end of wrapped function
-}
-
-// Uses city coordinates to get current weather forecast
-function getCurrentWeather(pullCurrentData) {
-  var { lat, lon } = pullCurrentData
-  var city = pullCurrentData.name
-  var apiCoordinatesURL = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=b98ec477e026dbcba46222f669c18788&units=imperial`
-  fetch(apiCoordinatesURL)
-    .then(function (response) {
-      // Stores response in JSON object
-      return response.json()
-        .then(function (data) {
-          // Retrieves first city result of our data object
-          getCurrentWeather(data[0])
-          getFutureWeather(data[0])
-          console.log(data.list[0])
+    fetch(requestURL)
+        .then(function (response) {
+            return response.json();
         })
+        .then (function (data){
+            var iconCode = data.weather[0].icon;
+            var iconurl = `http://openweathermap.org/img/w/${iconCode}.png`;
+            currentCity.text(`${data.name} ⋆ ${currentDate}`);
+            currentCity.prepend(`<img id="t-icon" width="50px" height="50px"></img>`)
+            $('#t-icon').attr('src', iconurl)
+            currentTemp.text(`Temp: ${data.main.temp} °F`)
+            currentWind.text(`Wind Speed: ${data.wind.speed} mph`)
+            currentHumid.text(`Humidity: ${data.main.humidity}%`)
+        })
+}
+
+//API call to fetch and display 5-day forecast in searched city
+function weatherWeek() {
+    var fiveDayURL = `https://api.openweathermap.org/data/2.5/forecast?q=${city.val()}&appid=${APIkey}&units=imperial`
+
+    fetch(fiveDayURL)
+        .then (function (response) {
+            return response.json();
+        })
+        .then (function (data){ 
+            $("h4").removeClass("d-none")
+            $("#5-day").removeClass("d-none")
+            $('#day-1').text(dayjs.unix(data.list[7].dt).format('M/D/YY'))
+            $('#day-2').text(dayjs.unix(data.list[15].dt).format('M/D/YY'))
+            $('#day-3').text(dayjs.unix(data.list[23].dt).format('M/D/YY'))
+            $('#day-4').text(dayjs.unix(data.list[31].dt).format('M/D/YY'))
+            $('#day-5').text(dayjs.unix(data.list[39].dt).format('M/D/YY'))
+            $('#T1').text(`Temp: ${data.list[7].main.temp}°F`)
+            $('#T2').text(`Temp: ${data.list[15].main.temp}°F`)
+            $('#T3').text(`Temp: ${data.list[23].main.temp}°F`)
+            $('#T4').text(`Temp: ${data.list[31].main.temp}°F`)
+            $('#T5').text(`Temp: ${data.list[39].main.temp}°F`)
+            $('#W1').text(`Wind: ${data.list[7].wind.speed} mph`)
+            $('#W2').text(`Wind: ${data.list[15].wind.speed} mph`)
+            $('#W3').text(`Wind: ${data.list[23].wind.speed} mph`)
+            $('#W4').text(`Wind: ${data.list[31].wind.speed} mph`)
+            $('#W5').text(`Wind: ${data.list[39].wind.speed} mph`)
+            $('#H1').text(`Humidity: ${data.list[7].main.humidity}%`)
+            $('#H2').text(`Humidity: ${data.list[15].main.humidity}%`)
+            $('#H3').text(`Humidity: ${data.list[23].main.humidity}%`)
+            $('#H4').text(`Humidity: ${data.list[31].main.humidity}%`)
+            $('#H5').text(`Humidity: ${data.list[39].main.humidity}%`)
+            $('#day-1').append(`<img src='http://openweathermap.org/img/w/${data.list[7].weather[0].icon}.png' width="50px" height="50px"></img>`)
+            $('#day-2').append(`<img src='http://openweathermap.org/img/w/${data.list[15].weather[0].icon}.png' width="50px" height="50px"></img>`)
+            $('#day-3').append(`<img src='http://openweathermap.org/img/w/${data.list[23].weather[0].icon}.png' width="50px" height="50px"></img>`)
+            $('#day-4').append(`<img src='http://openweathermap.org/img/w/${data.list[31].weather[0].icon}.png' width="50px" height="50px"></img>`)
+            $('#day-5').append(`<img src='http://openweathermap.org/img/w/${data.list[39].weather[0].icon}.png' width="50px" height="50px"></img>`)
+        })
+
+}
+function saveSearch() {
+    $("#saved-search").append(`<a href="#" class="my-2 btn custom-btn">${city.val()}</a>`);
+    var newCity = city.val()
+    if (localStorage.getItem('city')== null){
+        localStorage.setItem('city', '[]')
+    }
+    var savedCities = JSON.parse(localStorage.getItem('city'));
+    savedCities.push(newCity);
+    localStorage.setItem('city', JSON.stringify(savedCities))
+}
+
+function showSearches(){
+    var savedCities = JSON.parse(localStorage.getItem('city'));
+    savedCities.forEach( savedCities =>{
+        $("#saved-search").append(`<a href="#" class="my-2 btn custom-btn">${savedCities}</a>`)
     })
 }
 
+function getWeather(){
+    weatherToday();
+    weatherWeek();
+    saveSearch()
+}
 
-// Click Event for Gray History Buttons
+searchBtn.on("click", getWeather)
+
+//Re-runs previous searches upon click
+savedSearches.on("click",function(event){
+    city.attr("value", event.target.text)
+    weatherToday()
+    weatherWeek()
+})
+
+showSearches()
